50d2163881a194ffdd36cfaafbf3b83d30bc7580
Dongbin Na <ndb796@naver.com>
2020-07-02 17:19:26 +0900

    Update __init__.py

--- models/custom_groupy/gconv/pytorch_gconv/__init__.py
+++ models/custom_groupy/gconv/pytorch_gconv/__init__.py
@@ -1,3 +1,1 @@
-import sys
-
-from splitgconv2d import P4ConvZ2, P4ConvP4, P4MConvZ2, P4MConvP4M
+from custom_groupy.gconv.pytorch_gconv.splitgconv2d import P4ConvZ2, P4ConvP4, P4MConvZ2, P4MConvP4M
