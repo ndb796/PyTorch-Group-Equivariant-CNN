dfec3dc0dac9b44a701e09df02ea9436193a61fd
Dongbin Na <ndb796@naver.com>
2020-07-02 15:25:32 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from torch.utils.data import Dataset
 from PIL import Image
 import numpy as np
 
+
 class MyDataset(Dataset):
     def __init__(self, data, targets, transform=None):
         self.data = data
@@ -44,3 +45,25 @@ def AIGS_Dataset(transform_train, transform_test):
     testset = MyDataset(test_inputs, test_targets, transform=transform_test)
 
     return trainset, testset
+
+
+def mixup_data(x, y, alpha=1.0, use_cuda=True):
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+
+    batch_size = x.size()[0]
+    if use_cuda:
+        index = torch.randperm(batch_size).cuda()
+    else:
+        index = torch.randperm(batch_size)
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a, y_b = y, y[index]
+
+    return mixed_x, y_a, y_b, lam
+
+
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
