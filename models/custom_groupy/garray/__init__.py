a0c1ec686a1e3a9656a69208d84c58c224c9c27f
Dongbin Na <ndb796@naver.com>
2020-07-02 17:17:10 +0900

    Update __init__.py

--- models/custom_groupy/garray/__init__.py
+++ models/custom_groupy/garray/__init__.py
@@ -1,5 +1,5 @@
-from Z2_array import Z2Array
-from p4_array import P4Array
-from p4m_array import P4MArray
-from C4_array import C4Array, C4Group
-from D4_array import D4Array, D4Group
+from custom_groupy.garray.Z2_array import Z2Array
+from custom_groupy.garray.p4_array import P4Array
+from custom_groupy.garray.p4m_array import P4MArray
+from custom_groupy.garray.C4_array import C4Array, C4Group
+from custom_groupy.garray.D4_array import D4Array, D4Group
